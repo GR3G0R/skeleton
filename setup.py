5aa725128c03d38e6882ad48ad3f5b04af02c0d5
Gregory <Gregory@Gregorys-Air.home>
2015-03-08 21:28:18 -0400

    Adds web.py module at first attemp to run app

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+config = {
+  'description' : 'My Project',
+  'author' : 'Gregory Delpe',
+  'url': ' ',
+  'download url' : ' ',
+  'author_email' : ' ',
+  'version' : '0.1',
+  'install requires': ['nose'],
+  'packages' : ['Name'],
+  'scripts' : [],
+  'name': 'project1',
+
+}
